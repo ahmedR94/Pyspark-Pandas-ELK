bdcdd0af33b07dee7c37b761ec60b4d7483800d7
Ahmed Ridene <ahmedridene5@gmail.com>
2023-06-16 09:32:29 +0100

    build: first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+#### PYSPARK ELK Project
+
+to test the app localy:
+- bash start.sh
---
+++ start.sh
@@ -0,0 +1,4 @@
+docker-compose -f elk.yml up -d
+python3 -m venv my_env
+source ./my_env/bin/activate
+pip install -r requirements.txt
